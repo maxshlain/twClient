a9fb63afed7e48a848e76297ffc0d7ee97e3ccbf
Max Shlain <maxshlain@gmail.com>
2012-09-13 17:11:09 +0300

    begin

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace twClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_login = new System.Windows.Forms.TextBox();
+            this.txt_tweet = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_tweet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_login
+            // 
+            this.txt_login.Location = new System.Drawing.Point(55, 35);
+            this.txt_login.Multiline = true;
+            this.txt_login.Name = "txt_login";
+            this.txt_login.Size = new System.Drawing.Size(137, 46);
+            this.txt_login.TabIndex = 0;
+            // 
+            // txt_tweet
+            // 
+            this.txt_tweet.Location = new System.Drawing.Point(55, 106);
+            this.txt_tweet.Multiline = true;
+            this.txt_tweet.Name = "txt_tweet";
+            this.txt_tweet.Size = new System.Drawing.Size(137, 68);
+            this.txt_tweet.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(198, 44);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_tweet
+            // 
+            this.btn_tweet.Location = new System.Drawing.Point(198, 151);
+            this.btn_tweet.Name = "btn_tweet";
+            this.btn_tweet.Size = new System.Drawing.Size(75, 23);
+            this.btn_tweet.TabIndex = 3;
+            this.btn_tweet.Text = "tweet";
+            this.btn_tweet.UseVisualStyleBackColor = true;
+            this.btn_tweet.Click += new System.EventHandler(this.btn_tweet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 223);
+            this.Controls.Add(this.btn_tweet);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_tweet);
+            this.Controls.Add(this.txt_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_login;
+        private System.Windows.Forms.TextBox txt_tweet;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_tweet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Twitterizer;
+
+namespace twClient
+{
+    public partial class Form1 : Form
+    {
+        string consumerKey = Properties.Settings.Default.consumerKey;
+        string consumerSecret = Properties.Settings.Default.consumerSecret;
+        string callbackAddy = "oob";
+        OAuthTokenResponse tokenResponse2 = new OAuthTokenResponse();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            OAuthTokenResponse tokenResponse = new OAuthTokenResponse();
+            tokenResponse = Twitterizer.OAuthUtility.GetRequestToken(consumerKey, consumerSecret, callbackAddy);
+            string pin = "";
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.pinSaved))
+            {
+                // Need to check if the user is a valid user.
+
+                //OAuthTokenResponse tokenResponse = new OAuthTokenResponse();
+                tokenResponse = Twitterizer.OAuthUtility.GetRequestToken(consumerKey, consumerSecret, callbackAddy);
+                txt_login.Text = "Token is:  " + tokenResponse.Token.ToString();
+
+                string target = "http://twitter.com/oauth/authorize?oauth_token=" + tokenResponse.Token;
+                try
+                {
+                    System.Diagnostics.Process.Start(target);
+                }
+                catch (System.ComponentModel.Win32Exception noBrowser)
+                {
+                    if (noBrowser.ErrorCode == -2147467259)
+                        MessageBox.Show(noBrowser.Message);
+                }
+                catch (System.Exception other)
+                {
+                    MessageBox.Show(other.Message);
+                }
+
+                pin = "2279180"; //This WILL NOT WORK. User needs to enter the PIN
+                EnterPin enterpin = new EnterPin();
+                enterpin.ShowDialog(); //show dialog causes it to wait for user input. Show() would not work
+                pin = Properties.Settings.Default.pin;
+            }
+            else {
+                pin = Properties.Settings.Default.pinSaved;
+            }
+
+            
+             tokenResponse2 = OAuthUtility.GetAccessToken(consumerKey, consumerSecret, tokenResponse.Token, pin);
+             txt_login.Text = "App "+ tokenResponse2.ScreenName.ToString()+" access allowed.";
+
+        }
+
+        private void btn_tweet_Click(object sender, EventArgs e)
+        {
+
+            OAuthTokens tokens = new OAuthTokens();
+            //tokens.AccessToken = tokenResponse2.Token; // 15083553-ytdhRbkwCAavvQXpbW01NtKbwCAuOXcsZIBpLJlvw
+            //tokens.AccessTokenSecret = tokenResponse2.TokenSecret; //f8dnabiXlmnilGyUllRrgwQX8pL85sCunOf5OvOXf0
+            //tokens.ConsumerKey = consumerKey; // YWm9zyLW7gFSvV2EsVbQ
+            //tokens.ConsumerSecret = consumerSecret; // W9d57yqhAbLV8rZB1syx3V06U6kmDtCNxxlaFkGpSg
+
+
+            tokens.AccessToken = "15083553-ytdhRbkwCAavvQXpbW01NtKbwCAuOXcsZIBpLJlvw";
+            tokens.AccessTokenSecret = "f8dnabiXlmnilGyUllRrgwQX8pL85sCunOf5OvOXf0";
+            tokens.ConsumerKey = "YWm9zyLW7gFSvV2EsVbQ";
+            tokens.ConsumerSecret = "W9d57yqhAbLV8rZB1syx3V06U6kmDtCNxxlaFkGpSg";
+
+            //TwitterStatus tweetResponse = new TwitterStatus();
+            TwitterResponse<TwitterStatus> tweetResponse = TwitterStatus.Update(tokens, txt_tweet.Text);
+            if (tweetResponse.Result == RequestResult.Success)
+            {
+                txt_tweet.AppendText(" Update Successful.");
+            }
+            else
+            {
+                txt_tweet.AppendText(" Update did not go through.");
+            }
+
+        }
+    }
+}
